f8cc11506dd4ea9cef77df76ed48fc05bdaaf186
David Inglés <dvdingles@hotmail.com>
2022-12-14 19:44:20 +0100

    odio ts

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,11 @@
 import {Button} from '@nextui-org/react'
+import { Layout } from '../components/layout/Layout'
 import styles from './Home.module.css'
 
 export default function Home() {
   return (
       <>
+        <Layout />
         <div className={styles.prueba}>
           Hola
         </div>
