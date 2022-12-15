83a8d3c2b6e6f3debb6a92fed67e2b9ecead3ae3
dvdapogeo <dvdingles@hotmail.com>
2022-12-15 07:37:04 +0100

    foto scalable

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,8 @@
 import {Button} from '@nextui-org/react'
 import styles from './Home.module.css'
 import { Layout } from '../components/layout/Layout';
+import Image from 'next/image';
+import  nave  from '../public/nave.jpg'
 
 export default function Home() {
   return (
@@ -8,13 +10,32 @@ export default function Home() {
         <Layout title='Paula Inglés'>
        
           <div className={styles.prueba}>
-            Hola
+            Auxiliar Manipuladora del Cartón
           </div>
 
           <div className={styles.david}>
             <Button color={'gradient'}>
-              Hola Mundo
+              Inicio
             </Button>
+
+            <div style={{display: 'block', position: 'relative'}}>
+              <Image
+                alt='nave'
+                // src='/nave.jpg'
+                src={nave}
+                // fill
+                width={333}
+                height={333}
+                // sizes='100%'
+                style={{
+                  objectFit: 'contain',
+                  transition: 'all 1s ease',
+                  transform: 'scale',
+                }}
+                className={styles.nave}
+              />
+            </div>
+              
           </div>
           
         </ Layout>
